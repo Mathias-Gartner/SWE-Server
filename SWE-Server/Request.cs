08c08c65f2d163c1f2f6a4884affb7b4e477cc07
Mathias Gartner <mathias.gartner@technikum-wien.at>
2013-10-17 15:54:33 +0200

    added POST request parsing

--- SWE-Server/Request.cs
+++ SWE-Server/Request.cs
@@ -12,64 +12,107 @@ namespace SWE_Server
     class Request
     {
         private Socket _socket;
+        private StreamReader _reader;
 
-        public IDictionary<string, string> Header { get; private set; }
+        public Url Url { get; private set; }
+        public IReadOnlyDictionary<string, string> Header { get; private set; }
+        public IReadOnlyDictionary<string, string> PostData { get; private set; }
 
         public Request(Socket socket)
         {
-            IDictionary<string, string> header = new Dictionary<string,string>();
+            IDictionary<string, string> header = new Dictionary<string, string>();
             _socket = socket;
 
-            NetworkStream stream = new NetworkStream(_socket);
-            StreamReader reader = new StreamReader(stream);
-
-            Url url = null;
-
-            while (true)
+            NetworkStream stream = null;
+            try
             {
-                string line = "";
-                try
-                {
-                    line = reader.ReadLine();
-                }
-                catch (IOException e)
-                {
-                    Console.WriteLine("Error while reading request: " + e.Message);
-                }
-                
-                if (string.IsNullOrEmpty(line))
-                    break;
+                stream = new NetworkStream(_socket);
+                _reader = new StreamReader(stream);
 
-                if (url == null)
+                while (true)
                 {
-                    var buffer = line.Split(' ');
-
-                    url = new Url(buffer[0], buffer[1]);
-                }
-                else
-                {
-                    var parts = line.Split(": ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                    if (parts.Length > 1)
+                    string line = "";
+                    try
                     {
-                        header.Add(parts[0], parts[1]);
+                        line = _reader.ReadLine();
                     }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("Error while reading request: " + e.Message);
+                    }
+
+                    if (string.IsNullOrEmpty(line))
+                        break;
+
+                    if (Url == null)
+                    {
+                        var buffer = line.Split(' ');
+
+                        Url = new Url(buffer[0], buffer[1]);
+                    }
+                    else
+                    {
+                        var parts = line.Split(": ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                        if (parts.Length > 1)
+                        {
+                            header.Add(parts[0].ToLower(), parts[1].ToLower());
+                        }
+                    }
+
+                    Console.WriteLine(line);
                 }
 
-                Console.WriteLine(line);
+                Header = new ReadOnlyDictionary<string, string>(header);
+
+                if (Url != null && Url.Method == Url.MethodType.POST)
+                {
+                    ProcessPostData();
+                }
             }
-
-            Header = new ReadOnlyDictionary<string, string>(header);
-
-            if (url != null && url.Method == Url.MethodType.POST)
+            catch (IOException e)
             {
-                ProcessPostData();
+                Console.WriteLine("Error receiving request: " + e.Message);
+            }
+            catch (NotSupportedException)
+            {
+                if (Url != null && Url.Method == Url.MethodType.POST)
+                    Console.WriteLine("Content-Type not supported");
+                else
+                    Console.WriteLine("Request not supported");
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
             }
         }
 
         private void ProcessPostData()
         {
-            throw new NotImplementedException();
-        }
+            switch (Header["content-type"])
+            {
+                case "multipart/form-data":
+                    throw new NotSupportedException();
+                case "application/x-www-form-urlencoded":
+                    string line;
+                    line = _reader.ReadLine();
+                    if (String.IsNullOrEmpty(line))
+                        break;
 
-     }
+                    var dictionary = new Dictionary<string, string>();
+                    var args = line.Split('&');
+                    foreach (var arg in args)
+                    {
+                        var parts = arg.Split('=');
+                        if (parts.Length < 2)
+                            continue;
+                        dictionary.Add(parts[0], parts[1]);
+                    }
+                    PostData = new ReadOnlyDictionary<string, string>(dictionary);
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
 }
